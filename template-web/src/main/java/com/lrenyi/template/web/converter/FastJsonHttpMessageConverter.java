7a9c428edeae071431e08a531567a2661624acd4
RenYiChun <lrenyi.com@gmail.com>
2024-09-27 15:23:36 +0800

    解决FastJson插件会转换字节数组的类型

--- template-web/src/main/java/com/lrenyi/template/web/converter/FastJsonHttpMessageConverter.java
+++ template-web/src/main/java/com/lrenyi/template/web/converter/FastJsonHttpMessageConverter.java
@@ -4,10 +4,19 @@ import com.alibaba.fastjson2.JSON;
 import java.io.Reader;
 import java.io.Writer;
 import java.lang.reflect.Type;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.AbstractJsonHttpMessageConverter;
 import org.springframework.lang.NonNull;
 
 public class FastJsonHttpMessageConverter extends AbstractJsonHttpMessageConverter {
+    
+    @Override
+    public boolean canWrite(Type type, Class<?> clazz, MediaType mediaType) {
+        if (byte[].class.isAssignableFrom(clazz) || clazz.isPrimitive() || String.class.isAssignableFrom(clazz)) {
+            return false;
+        }
+        return super.canWrite(type, clazz, mediaType);
+    }
     
     @Override
     @NonNull
