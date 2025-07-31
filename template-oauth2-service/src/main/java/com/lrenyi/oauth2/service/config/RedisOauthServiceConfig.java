a35eab4bcfa1ac29f90d34094211d49fe578f9ba
RenYiChun <lrenyi.com@gmail.com>
2025-07-31 11:51:51 +0800

    增加默认的redis bean

--- template-oauth2-service/src/main/java/com/lrenyi/oauth2/service/config/RedisOauthServiceConfig.java
+++ template-oauth2-service/src/main/java/com/lrenyi/oauth2/service/config/RedisOauthServiceConfig.java
@@ -3,10 +3,14 @@ package com.lrenyi.oauth2.service.config;
 import com.lrenyi.oauth2.service.oauth2.redis.RedisOAuth2AuthorizationService;
 import com.lrenyi.oauth2.service.oauth2.redis.RedisRegisteredClientRepository;
 import java.util.List;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.oauth2.server.servlet.OAuth2AuthorizationServerProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
@@ -26,5 +30,23 @@ public class RedisOauthServiceConfig {
         OAuth2AuthorizationServerPropertiesMapper mapper = new OAuth2AuthorizationServerPropertiesMapper(properties);
         List<RegisteredClient> registeredClients = mapper.asRegisteredClients();
         return new RedisRegisteredClientRepository(brt, registeredClients.toArray(new RegisteredClient[0]));
+    }
+    
+    @Bean
+    @ConditionalOnMissingBean
+    public RedisTemplate<String, byte[]> byteArrayRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, byte[]> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        
+        // 设置key的序列化方式
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        
+        // 设置value的序列化方式 - 对于byte[]，使用原始字节序列化器
+        template.setValueSerializer(RedisSerializer.byteArray());
+        template.setHashValueSerializer(RedisSerializer.byteArray());
+        
+        template.afterPropertiesSet();
+        return template;
     }
 }
